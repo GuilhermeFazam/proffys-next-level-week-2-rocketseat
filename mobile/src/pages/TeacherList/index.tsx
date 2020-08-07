091a169eed9c32bf63378ecf5b1812cbd012b38e
Guilherme Althman Fazam <guilherme.fazam@neoris.com>
2020-08-07 18:30:13 -0300

    Proffys - NLW#2 - Dia 4 - Expo AppMobile - Criando as naveções TABS entre Proffys e Favoritos - Rocketseat

---
+++ mobile/src/pages/TeacherList/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+import PageHeader from '../../components/PageHeader';
+import styles from './styles';
+
+const TeacherList: React.FC = () => {
+    return (
+        <View style={styles.container}>
+            <PageHeader title="Proffys disponiveis" />
+        </View>
+    );
+};
+
+export default TeacherList;
